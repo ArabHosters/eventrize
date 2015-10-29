43145ca0bf526d49a0821a4370406508a30ee27d
Hazem Khaled <hazem.khaled@gmail.com>
2015-10-29 13:05:51 +0200

    Load tabs programmatically depend on config.son file

--- app/controllers/tabs.js
+++ app/controllers/tabs.js
@@ -1,1 +1,6 @@
 var args = arguments[0] || {};
+
+// Load tabs depend on configs
+_.each(Alloy.CFG.tabs, function(tabName) {
+  $.tabs.addTab(Alloy.createController(tabName + '_tab/index').getView());
+});
