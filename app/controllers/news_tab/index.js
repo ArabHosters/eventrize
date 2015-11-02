8ea4acafdb7b70b10f27ed1a3859528d4cae8717
Sherif Magdy <s.magdy@arabhosters.com>
2015-11-02 13:29:22 +0200

    implemnt pull to refresh

--- app/controllers/news_tab/index.js
+++ app/controllers/news_tab/index.js
@@ -1,20 +1,19 @@
-var args = arguments[0] || {};
-
-
-
-/*
-for (var i = 0; i < 4; i++) {
-
-	var newsRow = Alloy.createModel('news', {
-		image : "http://www.zastavki.com/pictures/originals/2013/Photoshop_Image_of_the_horse_053857_.jpg",
-		title : "Tiltle1",
-		short_desc : "AAAAA",
-		date : new Date()
-	});
-	
-	newsRow.save();
-
+var args=arguments[0]|| {};
+function myRefresher(e) {
+Alloy.Collections.news.fetch({
+success:e.hide,error:e.hide
+});
+}
+$.ptr.refresh();
+/*for (var i = 0; i < 2; i++) {
+var newsRow = Alloy.createModel('news', {
+image : "http://i.imgur.com/yxu9fZ1.jpg",
+title : "Tiltle1",
+short_desc : "AAAAA",
+date : new Date()
+});
+newsRow.save();
 }*/
+//Alloy.Collections.news.fetch();
 
 
-Alloy.Collections.news.fetch();
