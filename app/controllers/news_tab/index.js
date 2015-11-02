eae01a77a2b6559ceb73a02607c908b1ff64370e
Sherif Magdy <s.magdy@arabhosters.com>
2015-11-02 23:19:34 +0200

    Fix misspell in infinity scroll implementation

--- app/controllers/news_tab/index.js
+++ app/controllers/news_tab/index.js
@@ -1,48 +1,33 @@
-var args=arguments[0]|| {};
+var args = arguments[0] || {};
+
 //implement pull to refresh
 function myRefresher(e) {
-Alloy.Collections.news.fetch({
-success:e.hide,error:e.hide
-});
+	setTimeout(function(){
+		e.hide();
+	}, 3000);
 }
+
 $.ptr.refresh();
-//------------------------------------------------------------------------------
-/*for (var i = 0; i < 2; i++) {
-var newsRow = Alloy.createModel('news', {
-image : "http://i.imgur.com/yxu9fZ1.jpg",
-title : "Tiltle1",
-short_desc : "AAAAA",
-date : new Date()
-});
-newsRow.save();
-}*/
-//Alloy.Collections.news.fetch();
-//--------------------------------------------------------------------------------
-// NOTE: do not use: var myLoader = function(e) {
+
 function myLoader(e) {
-
-var ln=Alloy.Collection.news.models.length;
-
-Alloy.Collections.news.fetch({
-// whatever your sync adapter needs to fetch the next page
-
-data: {
-offset:ln
-},
-// don't reset the collection, but add to it
-
-add:true,success: function(col) {
-// call done() when we received last page - else success()
-(col.models.length===ln)?e.done():e.success();
-},
-// call error() when fetch fails
-error: function(col) {
-// pass optional error message to display
-e.error(L('isError','Tap to try again...'));
+	var ln = Alloy.Collections.news.models.length;
+	Alloy.Collections.news.fetch({
+		// whatever your sync adapter needs to fetch the next page
+		data : {
+			offset : ln
+		},
+		// don't reset the collection, but add to it
+		add : true,
+		success : function(col) {
+			// call done() when we received last page - else success()
+			(col.models.length === ln) ? e.done() : e.success();
+		},
+		// call error() when fetch fails
+		error : function(col) {
+			// pass optional error message to display
+			e.error(L('isError', 'Tap to try again...'));
+		}
+	});
 }
-});
-}
+
 $.is.init($.list);
-//-------------------------------------------------------------------------------
-
-
