26751647a869e793097e61cb3bc27163325c05a0
Hazem Khaled <hazem.khaled@gmail.com>
2015-11-03 18:38:06 +0200

    Fix: infinity scroll

--- app/controllers/posts_tab/index.js
+++ app/controllers/posts_tab/index.js
@@ -2,33 +2,37 @@ var args = arguments[0] || {};
 
 //implement pull to refresh
 function myRefresher(e) {
-	Alloy.Collections.posts.fetch({
-		success : e.hide,
-		error : e.hide
-	});
+  Alloy.Collections.posts.fetch({
+    urlparams: {
+      per_page: Alloy.CFG.posts.per_page,
+      page: 1
+    },
+    success: e.hide,
+    error: e.hide
+  });
 }
 
 $.ptr.refresh();
 
 function myLoader(e) {
-	var ln = Alloy.Collections.posts.models.length;
-	Alloy.Collections.posts.fetch({
-		// whatever your sync adapter needs to fetch the next page
-		data : {
-			//offset : ln
-		},
-		// don't reset the collection, but add to it
-		add : true,
-		success : function(col) {
-			// call done() when we received last page - else success()
-			(col.models.length === ln) ? e.done() : e.success();
-		},
-		// call error() when fetch fails
-		error : function(col) {
-			// pass optional error message to display
-			e.error(L('isError', 'Tap to try again...'));
-		}
-	});
+  var ln = Alloy.Collections.posts.models.length;
+  Alloy.Collections.posts.fetch({
+    // whatever your sync adapter needs to fetch the next page
+    urlparams: {
+      per_page: Alloy.CFG.posts.per_page,
+      page: parseInt(ln / 10) + 1
+    },
+    // don't reset the collection, but add to it
+    add: true,
+    silent: true,
+    returnExactServerResponse: true,
+    success: function(col) {
+      // call done() when we received last page - else success()
+      (col.models.length === ln) ? e.done(): e.success();
+    },
+    // call error() when fetch fails
+    error: e.error
+  });
 }
 
 $.is.init($.list);
