361a1018c2b04da2202333d0dd4f1aaf0ffde6c8
Hazem Khaled <hazem.khaled@gmail.com>
2015-11-03 22:28:37 +0200

    Share button

--- app/controllers/pages_tab/index.js
+++ app/controllers/pages_tab/index.js
@@ -5,3 +5,13 @@ $.contentLabel.text = args.data.content;
 if (args.data.featured_image) {
   $.featuredImageView.image = args.data.featured_image;
 }
+
+function shareButtonClicked(e) {
+  // share text status
+  require('com.alcoapps.socialshare').share({
+    status: args.data.title,
+    image: args.data.featured_image,
+    url: args.data.link,
+    androidDialogTitle: L('shareDialogTitle')
+  });
+}
