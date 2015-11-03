d28add338bbde014aa5f813ff0ae75c41f01ec53
Hazem Khaled <hazem.khaled@gmail.com>
2015-11-03 22:00:19 +0200

    Android now open child pages

--- app/controllers/android/pages_tab/index.js
+++ app/controllers/android/pages_tab/index.js
@@ -1,7 +1,8 @@
 Alloy.Collections.pages.fetch();
 
 function onItemClick(e) {
-  //var item = section[e.sectionIndex].getItemAt(e.itemIndex);
 
-  Alloy.createController('posts_tab/post', Alloy.Collections.pages.at(e.itemIndex).toJSON()).getView().open();
+  Alloy.createController('pages_tab/index', {
+    data: Alloy.Collections.pages.at(e.itemIndex).toJSON()
+  }).getView().open();
 }
