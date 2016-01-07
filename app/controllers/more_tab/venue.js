120122e4ea77ddf5825706ba3f87ec8a74c0c095
Hazem Khaled <hazem.khaled@gmail.com>
2016-01-07 10:22:47 +0200

    Style map view annotation

--- app/controllers/more_tab/venue.js
+++ app/controllers/more_tab/venue.js
@@ -1,19 +1,23 @@
 var Map = require('ti.map');
 
 $.mapView.region = {
-  latitude: Alloy.Globals.lastActiveEvent.latitude,
-  longitude: Alloy.Globals.lastActiveEvent.longitude,
-  zoom: Alloy.Globals.lastActiveEvent.mapZoom
+	latitude: Alloy.Globals.lastActiveEvent.latitude,
+	longitude: Alloy.Globals.lastActiveEvent.longitude,
+	zoom: Alloy.Globals.lastActiveEvent.mapZoom
 };
 
 // Add venue location
 var myAnnotation = Map.createAnnotation({
-  title: Alloy.Globals.lastActiveEvent.venueTitle,
-  subtitle: Alloy.Globals.lastActiveEvent.venueAddress,
-  latitude: Alloy.Globals.lastActiveEvent.latitude,
-  longitude: Alloy.Globals.lastActiveEvent.longitude,
-  leftButton: Alloy.Globals.lastActiveEvent.venueThumbnail,
-  myid: 1
+	latitude: Alloy.Globals.lastActiveEvent.latitude,
+	longitude: Alloy.Globals.lastActiveEvent.longitude,
+	myid: 1,
+	title: Alloy.Globals.lastActiveEvent.venueTitle,
+	subtitle: Alloy.Globals.lastActiveEvent.venueAddress,
+	leftView: Ti.UI.createImageView({
+		image: Alloy.Globals.lastActiveEvent.venueThumbnail,
+		width: 70,
+		height: 70
+	})
 });
+
 $.mapView.addAnnotation(myAnnotation);
-myAnnotation.fireEvent('click');
